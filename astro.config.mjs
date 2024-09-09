816bc2ae483d2f931ee30633ba051328d220c2c0
JesusSanchez25 <jsus3005@gmail.com>
2024-09-09 18:27:48 +0200

    seguimo probadno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 
 export default defineConfig({
-  output: 'server',
   adapter: vercel(),
   //integrations: [icon()],
 });
