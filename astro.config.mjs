edf31724cac8c4b755296b12946a3ade406985bd
JesusSanchez25 <jsus3005@gmail.com>
2024-09-09 18:14:10 +0200

    configurando vercel con astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import vercel from '@astrojs/vercel/static';
+
 
 export default defineConfig({
+  output: 'server',
   adapter: vercel({
     imageService: true,
   }),
