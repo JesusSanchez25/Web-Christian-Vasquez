1dcce2de81709f22337aee754598cf4d3ffd033c
JesusSanchez25 <jsus3005@gmail.com>
2024-09-09 18:06:04 +0200

    seguimo aqui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
-export default defineConfig({
-  integrations: [icon()],
-});
-
 adapter: vercel({
   imageService: true,
+});
+
+export default defineConfig({
+  integrations: [icon()],
 });
