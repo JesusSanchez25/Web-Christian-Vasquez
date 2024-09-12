6e436adcaa9a6b0fa3232d851786416a364c5201
JesusSanchez25 <jsus3005@gmail.com>
2024-09-12 10:20:33 +0200

    pre-bugFix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-import vercel from '@astrojs/vercel/static';
-
+//import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  adapter: vercel(),
-  integrations: [icon()],
+ // adapter: vercel(),
+  integrations: [icon()]
 });
